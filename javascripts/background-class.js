918258ca791d076207e2b2632404a73e84a97b8b
bill <billsearle@me.com>
2014-08-29 20:17:55 +1000

    fixed js if statement to add css class

--- javascripts/background-class.js
+++ javascripts/background-class.js
@@ -1,6 +1,8 @@
-if ($.browser.mobile === true) {
-	console.log('true')
-	$('#background').addClass('mobile-wrapper');
-} else {
-console.log('false');
-};
+$(document).ready(function() {
+	if ($.browser.mobile === true) {
+		console.log('false')
+		$('#background').addClass('mobile-wrapper');
+	} else {
+	console.log('true');
+	};
+});
