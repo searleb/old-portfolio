fb28e1597881a9d938eafd4866c941edbf4f5bf0
bill <billsearle@me.com>
2014-08-29 21:05:17 +1000

    landscape media queries, removed userAgent detect, blurgh

--- javascripts/background-class.js
+++ javascripts/background-class.js
@@ -1,8 +1,7 @@
-$(document).ready(function() {
-	if ($.browser.mobile === true) {
-		console.log('false')
-		$('#background').addClass('mobile-wrapper');
-	} else {
-	console.log('true');
-	};
-});
+$( document ).ready(function() {      
+    var isMobile = window.matchMedia("only screen and (max-width: 1024px");
+
+    if (isMobile.matches) {
+        $('#background').addClass('mobile-wrapper');
+    }
+ });
