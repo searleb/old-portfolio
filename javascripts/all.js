7558fe0967c7eebf79dc342dcd844dbee3f98c39
bill <billsearle@me.com>
2014-08-29 21:30:06 +1000

    media queries redone to handle phone and tablet in both orientations

--- javascripts/all.js
+++ javascripts/all.js
@@ -9788,13 +9788,16 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 }
 
 })( window );
-$( document ).ready(function() {      
-    var isMobile = window.matchMedia("only screen and (max-width: 1024px");
+// media queries based on iphone and ipad sizes
+$(document).ready(function() {      
+    var isMobile = window.matchMedia("only screen and (min-device-width: 320px) and (max-device-width: 568px)");
+    var isTablet = window.matchMedia("only screen and (min-device-width: 768px) and (max-device-width: 1024px)");
 
-    if (isMobile.matches) {
+    if ((isMobile.matches) || (isTablet.matches))  {
         $('#background').addClass('mobile-wrapper');
     }
  });
+
 
 
 
--- javascripts/background-class.js
+++ javascripts/background-class.js
@@ -1,7 +1,10 @@
-$( document ).ready(function() {      
-    var isMobile = window.matchMedia("only screen and (max-width: 1024px");
+// media queries based on iphone and ipad sizes
+$(document).ready(function() {      
+    var isMobile = window.matchMedia("only screen and (min-device-width: 320px) and (max-device-width: 568px)");
+    var isTablet = window.matchMedia("only screen and (min-device-width: 768px) and (max-device-width: 1024px)");
 
-    if (isMobile.matches) {
+    if ((isMobile.matches) || (isTablet.matches))  {
         $('#background').addClass('mobile-wrapper');
     }
  });
+
