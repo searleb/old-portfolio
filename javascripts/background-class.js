c36575d013071c4cb32de0cf539169aec1a8dcb9
bill <billsearle@me.com>
2014-09-01 13:04:14 +1000

    mobile js for bg

--- javascripts/background-class.js
+++ javascripts/background-class.js
@@ -1,10 +1,10 @@
-// // media queries based on iphone and ipad sizes
-//     var isMobile = window.matchMedia("only screen and (min-device-width: 320px) and (max-device-width: 568px)");
-//     var isTablet = window.matchMedia("only screen and (min-device-width: 768px) and (max-device-width: 1024px)");
+// media queries based on iphone and ipad sizes
+    var isMobile = window.matchMedia("only screen and (min-device-width: 320px) and (max-device-width: 568px)");
+    var isTablet = window.matchMedia("only screen and (min-device-width: 768px) and (max-device-width: 1024px)");
 
-// $(document).ready(function() {      
-//     if ((isMobile.matches) || (isTablet.matches))  {
-//         $('#background').addClass('mobile-wrapper');
-//     }
-//  });
+$(document).ready(function() {      
+    if ((isMobile.matches) || (isTablet.matches))  {
+        $('#background').addClass('mobile-wrapper');
+    }
+ });
 
