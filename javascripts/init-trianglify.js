2cb261e3a3cc8d729a5ce569db4d46dec451050e
bill <billsearle@me.com>
2014-09-01 12:36:53 +1000

    refactored svg

--- javascripts/init-trianglify.js
+++ javascripts/init-trianglify.js
@@ -1,7 +1,8 @@
-$(document).ready(function(){
-	var t = new Trianglify({
-		noiseIntensity: 0,
-	});
+var t = new Trianglify({
+	noiseIntensity: 0,
+});
+
+var svgDraw = function (t){
 	var width = $(document).width();
 	var height = $(document).height();
 	var pattern = t.generate(width, height);
@@ -11,5 +12,11 @@ $(document).ready(function(){
 	$('#contact').css({
 		'background': pattern.dataUrl
 	});
-})
-;
+};
+
+$(document).ready(function(){
+	svgDraw(t);
+});
+$(window).resize(function() {
+ svgDraw(t);
+});
