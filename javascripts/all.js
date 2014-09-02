dbc614f1482a74943a7135fb9d6729ef10de1f2e
bill <billsearle@me.com>
2014-09-03 00:26:34 +1000

    google analytics

--- javascripts/all.js
+++ javascripts/all.js
@@ -10033,6 +10033,14 @@ $(window).resize(function() {
 // getMoney();
 
 // });
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-44359005-1', 'auto');
+  ga('send', 'pageview');
 // media queries based on iphone and ipad sizes
     var isMobile = window.matchMedia("only screen and (min-device-width: 320px) and (max-device-width: 568px)");
     var isTablet = window.matchMedia("only screen and (min-device-width: 768px) and (max-device-width: 1024px)");
