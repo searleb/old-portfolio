f615890a2b138aad9b090c5a406915636795492f
bill <billsearle@me.com>
2014-09-07 20:55:24 +1000

    background fade

--- javascripts/init-trianglify.js
+++ javascripts/init-trianglify.js
@@ -1,32 +1,111 @@
-var t = new Trianglify({
-	noiseIntensity: 0,
+// set #background-* to full window height and fade in the body
+$(document).ready(function(){
+	var width = $(window).width();
+	var height = $(window).height();
+		$('#background-container').css({
+			'min-width': width,
+			'min-height': height
+		});
+		$('#background-1').css({
+			'min-width': width,
+			'min-height': height
+		});
+		$('#background-2').css({
+			'min-width': width,
+			'min-height': height
+		});
 });
 
-var svgDraw = function (t){
-	var width = $(document).width();
-	var height = $(document).height();
-	var pattern = t.generate(width, height);
-	$('#background').css({
-		'background': pattern.dataUrl
+var svg = {};
+var draw = 1
+
+var svgNew = function(){
+	svg.t = new Trianglify({
+		noiseIntensity: 0,
 	});
-	$('#contact').css({
-		'background': pattern.dataUrl
+	svg.width = $(window).width();
+	svg.height = $(window).height();
+	svg.pattern = svg.t.generate(svg.width, svg.height);
+
+	if (draw === 1) {
+		svgDraw1();
+	} else {
+		svgDraw2();
+	}
+};
+
+var svgDraw1 = function (resize){
+	draw = 2
+		if (resize === 'resize') {	
+			svg.pattern = svg.t.generate(svg.width, svg.height);
+			$('#background-1').css({
+			'min-width': svg.width,
+			'min-height': svg.height,
+			'background': svg.pattern.dataUrl
+		});
+	} else {
+	$('#background-1').css({
+		'background': svg.pattern.dataUrl
 	});
+		fade1();
+	};
+
+	// button hover
 $('.btn').hover(
   function() {
     $( this ).css({
-    	'background-image': pattern.dataUrl
+    	'background-image': svg.pattern.dataUrl
     });
   }, function() {
     $( this ).css({
     	'background-image': 'none'
     });
   });
+}; // end svgDraw1
+
+
+var svgDraw2 = function(resize){
+	draw = 1
+		if (resize === 'resize') {	
+			svg.pattern = svg.t.generate(svg.width, svg.height);
+			$('#background-2').css({
+			'min-width': svg.width,
+			'min-height': svg.height,
+			'background': svg.pattern.dataUrl
+		});
+	} else {
+	$('#background-2').css({
+		'background': svg.pattern.dataUrl
+	});
+		fade2();
+	};
 };
 
+var fade1 = function(){
+	$('#background-1').velocity("fadeIn", { duration: 3000 });
+	$('#background-2').velocity("fadeOut", { duration: 4000 });
+};
+var fade2 = function(){
+	$('#background-2').velocity("fadeIn", { duration: 3000 });
+	$('#background-1').velocity("fadeOut", { duration: 4000 });
+};
+
+var recreateSvg = function(){
+ 	window.setInterval(svgNew, 5000)
+}
+
 $(document).ready(function(){
-	svgDraw(t);
+	svgNew();
+	recreateSvg();
 });
+
 $(window).resize(function() {
- svgDraw(t);
+	svg.width = $(window).width();
+	svg.height = $(window).height();
+	$('#background-container').css({
+		'min-width': svg.width,
+		'min-height': svg.height
+	});
+	svgDraw1('resize');
+	svgDraw2('resize');
 });
